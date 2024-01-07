abb79b82d6d27593a23fccdedd45410e0161344f
Nathan099-dev <tonininathan8@gmail.com>
2024-01-07 18:59:50 -0300

    Adicionando função

--- js/Cadastro.js
+++ js/Cadastro.js
@@ -1,10 +1,32 @@
-function cadastro_usuario(){
-    const nomeUsuário = nome.value;
-    const emailUsuario = email.value;
-    const senhaUsuario =  senha.value;
+const userName = document.getElementById('nome');
+const emailUsuario = document.getElementById('email')
+const password = document.getElementById('senha');
+const btn = document.querySelector('button');
 
-    if (nomeUsuário.value == null) {
-        window.alert('Insira seu nome neste campo')
-        nomeUsuário.style.borderColor = 'red'
+btn.onclick = function validar(event){
+    event.preventDefault();
+
+    if (userName.value == '') {
+        userName.classList.add('invalid');
+
+    }else{
+        userName.classList.remove('invalid');
     }
+
+    if (emailUsuario.value == '') {
+        emailUsuario.classList.add('invalid');
+
+    }else{
+        userName.classList.remove('invalid');
+    }
+
+    if (password.value == '') {
+        password.classList.add('invalid');
+
+    }else{
+        password.classList.remove('invalid');
+    }
+
 }
+
+validar();
