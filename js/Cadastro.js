e481f6755ae5b07acd8605405a915be62d77629e
Nathan099-dev <tonininathan8@gmail.com>
2024-05-28 18:38:15 -0300

    corrigindo código

--- js/Cadastro.js
+++ js/Cadastro.js
@@ -5,39 +5,39 @@ function cadastro_usuario(){
     const senha = document.getElementById('senha').value;
 
     //validação do nome de usuário
-    if (nome.value === '') {
+    if (nome.value == '') {
         alert('Insira o seu nome');
-        const nome =   document.getElementById('nome').style.border = "1px solid red";
+        nome = document.getElementById('nome').style.border = "1px solid red";
         return;
 
     //validação do email do  usuário
-    }else if (email.value === '') {
+    }else if (email.value = '') {
         alert('insira um email')
-        const email = document.getElementById('email').style.border = '1px solid red';
+        email = document.getElementById('email').style.border = '1px solid red';
         return;
 
-    }else  if (email.indexOf("@").value === -1 || email.indexOf('.').value === -1) {
+    }else  if (email.indexOf("@").value == -1 || email.indexOf('.').value == -1) {
         alert('Este endereço não é  um email válido');
-        const email = document.getElementById('email').style.border = '1px solid red'
+        email = document.getElementById('email').style.border = '1px solid red'
         return;
 
     //validação da senha do  usuário
-    }else if (senha.value === '') {
+    }else if (senha.value == '') {
         alert('Campo inválido');
-        const senha = document.getElementById('senha').border = '1px solid red'
+        senha = document.getElementById('senha').border = '1px solid red'
         return;
 
     }else if (senha.length < 8) {
         alert('Sua senha  precisa ter pelo menos oito caracteres')
-        const  senha  = document.getElementById('senha').style.border = '1px  solid red';
+        senha = document.getElementById('senha').style.border = '1px  solid red';
         return;
 
     //Caso todas as condições tenham sido  atendidas corretamente
     }else{
         alert('cadastro realizado com sucesso')
-        const nome =  document.getElementById('nome').style.border = "1px solid green";
-        const email = document.getElementById('email').style.border = "1px solid green";
-        const senha = document.getElementById('senha').style.border = "1px solid green";
+        nome =  document.getElementById('nome').style.border = "1px solid green";
+        email = document.getElementById('email').style.border = "1px solid green";
+        senha = document.getElementById('senha').style.border = "1px solid green";
 
         window.location.href = 'html/ login.html'
     }
