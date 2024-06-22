8c51f72e18d26414fcafe895f91bab2846474398
Nathan099-dev <tonininathan8@gmail.com>
2024-06-22 15:51:02 -0300

    deletando arquivo index.js

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const { response } = require("express");
-
-axios.get('https://www.googleapis.com/auth/books').then((response) => {
-    console.log(response.data);
-}).catch((error) => {
-    console.log(error);
-})
