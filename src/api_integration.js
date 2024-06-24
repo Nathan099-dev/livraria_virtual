6930e11a35542de2e4fad9daee7a70187c68e1bb
Nathan099-dev <tonininathan8@gmail.com>
2024-06-24 20:43:22 -0300

    mexendo na api no html e js

--- src/api_integration.js
+++ src/api_integration.js
@@ -1,7 +1,41 @@
-let api = 'https://www.googleapis.com/auth/books'
+let timeoutId;
 
- fetch(api).then(function returBooksData(req, res){
-    return res.json();
- }).then()
+function debounceSearch(){
+   if (timeoutId) {
+      clearTimeout(timeoutId)
+   }
 
- returBooksData()
+   timeoutId = setTimeout(() => {
+      searchBooks();
+   }, 300)
+}
+
+function searchBooks(){
+   let searchInput = document.getElementById('searchBookId').value;
+   if (searchInput.length < 3) {
+      return;
+   }
+   let apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + encodeURIComponent(searchInput);
+
+   fetch(apiUrl).then(response => response.json())
+   .then(data => displayResults(data))
+   .catch(error => console.error('Erro ao buscar dados', error))
+
+   
+}
+
+function displayResults(data){
+   let divResults = document.getElementById('result');
+   divResults.innerHTML = '';
+
+   if (data.totalItems > 0) {
+      data.items.forEach(livro => {
+         let titulo = livro.volumeInfo.title;
+         let autores  = livro.volumeInfo.authors.join(", ");
+         divResults.innerHTML += `<p><strong>${titulo}</strong> por ${autores}</p>`;
+      });
+   }else{
+      divResults.innerHTML += '<p>Nenhum livro  encotrado</p>';
+
+   }
+}
