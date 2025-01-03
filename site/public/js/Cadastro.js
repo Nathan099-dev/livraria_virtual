2bf2d5e44258d1ccf49d2484e0565be0d738ac11
Nathan099-dev <tonininathan8@gmail.com>
2025-01-03 15:34:20 -0300

    refatorando

--- site/public/js/Cadastro.js
+++ site/public/js/Cadastro.js
@@ -1,45 +1,44 @@
-//função para cadastro de usuário
-
 function cadastro_usuario(nome, email, senha){
     const usuarioCadastrado = 0;
     const nomeUsuario = nome.value;
     const emailUsuario = email.value;
     const senhaUsuario = senha.value;
 
-    //validação do nome de usuário
     if (nomeUsuario == '') {
         alert('Insira o seu nome');
         nome = document.getElementById('nome').style.border = "1px solid red";
         return;
-
-    //validação do email do  usuário
-    }else if (emailUsuario == '') {
+    }
+    
+    if (emailUsuario == '') {
         alert('insira um email')
         email = document.getElementById('email').style.border = '1px solid red';
         return;
-
-    }else  if (emailUsuario.indexOf("@") == -1 || emailUsuario.indexOf('.') == -1) {
+    }
+    
+    if (emailUsuario.indexOf("@") == -1 || emailUsuario.indexOf('.') == -1) {
         alert('Este endereço não é  um email válido');
         email = document.getElementById('email').style.border = '1px solid red'
         return;
-
-    //validação da senha do  usuário
-    }else if (senhaUsuario == '') {
+    }
+    
+    if (senhaUsuario == '') {
         alert('Campo inválido');
         senha =  document.getElementById('senha').style.border = '1px solid red'
         return;
 
-    }else if (senhaUsuario.length < 8) {
+    }
+    
+    if (senhaUsuario.length < 8) {
         alert('Sua senha  precisa ter pelo menos oito caracteres')
         senha = document.getElementById('senha').style.border = '1px  solid red';
         return;
 
-    //Caso todas as condições tenham sido  atendidas corretamente
     }else{
         alert('cadastro realizado com sucesso');
         usuarioCadastrado ++;
         window.location.href = 'login.html'
-        const boasVindas = window.alert('Bem vindo ao nosso site' + nomeUsuario)
-        return;
+        const boasVindas = window.alert('Bem vindo ao nosso site' + nomeUsuario);
+        return boasVindas;
     }
 }
